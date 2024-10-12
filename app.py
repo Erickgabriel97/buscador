dee18432b41d89b9e7dd281f53e4875fcc8a4f58
Erickgabriel97 <bieel97@icloud.com>
2024-10-12 13:55:42 -0300

    Adicionando animação de corações na resposta positiva

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
-import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "<h1 style='color: black;'>Olá, bem-vinda ao meu aplicativo!</h1>"
+def index():
+    return render_template('index.html')
+
+@app.route('/pergunta', methods=['POST'])
+def pergunta():
+    nome = request.form['nome']
+    if nome == 'Larissa Yumi Ferreira Ogata':
+        return render_template('pergunta.html')  # Renderiza a pergunta
+    else:
+        return "Nome incorreto. Tente novamente."
+
+@app.route('/resposta', methods=['POST'])
+def resposta():
+    resposta = request.form['resposta']
+    if resposta == 'sim':
+        return "Eu te amo! 💖"
+    else:
+        return "Você não pode clicar nesse botão! Tente novamente."
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Usa a porta do ambiente ou 5000 como padrão
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=5000)
